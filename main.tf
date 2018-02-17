f479f86a3bf3dfe155608e5d953e8c08f5d77d7f
Ciro S. Costa <ciro.costa@liferay.com>
2018-02-17 16:00:35 -0200

    Separates ami into its own file

--- main.tf
+++ main.tf
@@ -13,24 +13,6 @@ provider "aws" {
   profile = "${var.profile}"
 }
 
-# Pick the latest ubuntu artful (17.10) ami released by the
-# Canonical team.
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-artful-17.10-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
-
 module "networking" {
   source = "./networking"
   cidr   = "10.0.0.0/16"
